7d16c1e165d46833829c16905993280b2666548c
huhongxing <18510255246@163.com>
2020-01-30 16:13:54 +0800

    同步代码

---
+++ src/test/java/com/star/myeureka/MyeurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.star.myeureka;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyeurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
